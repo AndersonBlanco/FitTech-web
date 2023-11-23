efbd2a2eed3d0940011ff15b4ba4c7a228b6c1a3
AndersonBlanco <73806858+AndersonBlanco@users.noreply.github.com>
2023-11-23 20:33:20 +0000

    updated TF model parameters for accuracy

--- fit_tech/src/App.js
+++ fit_tech/src/App.js
@@ -3,9 +3,9 @@ import './App.css';
 import React, { useEffect, useReducer, useRef, useState, } from 'react';
 //import '@mediapipe/pose';
 import Webcam from 'react-webcam';
-import {OpenCvProvider, useOpenCv} from 'opencv-react';
-import {FilesetResolver, PoseLandmarker} from '@mediapipe/tasks-vision';
-import * as tf from '@tensorflow/tfjs';
+//import {OpenCvProvider, useOpenCv} from 'opencv-react';
+//import {FilesetResolver, PoseLandmarker} from '@mediapipe/tasks-vision';
+//import * as tf from '@tensorflow/tfjs';
 import * as posenet from '@tensorflow-models/posenet';
 import {drawKeypoints, drawSkeleton} from './utilities';
 
